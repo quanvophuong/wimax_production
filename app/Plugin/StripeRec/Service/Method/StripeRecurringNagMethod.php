c6df913c883232c89a9d0361cba957d8d33e1d43
flogfluid <flogfluid@gmail.com>
2023-05-29 21:31:37 +0900

    修正

--- app/Plugin/StripeRec/Service/Method/StripeRecurringNagMethod.php
+++ app/Plugin/StripeRec/Service/Method/StripeRecurringNagMethod.php
@@ -516,7 +516,11 @@ class StripeRecurringNagMethod implements PaymentMethodInterface
 	            'phases'        =>  $phases,
 	        ], $initial_price, $order_items[0]->getProduct()->getStripeProdId(), $interval,strtolower($this->Order->getCurrencyCode()));
             
+            if(array_key_exists('billing_cycle_anchor', $schedule_params['phases'][1])){
+                unset( $schedule_params['phases'][1]['billing_cycle_anchor']);
+            }
             $schedule_params['phases'][1]['trial_end'] =  $next_end->format('U');
+            log_info("StripeRecurringNagMethod---interval---trial_end:schedule_params " . print_r($schedule_params, true));
 		
 		}
         if($coupon_enable){
